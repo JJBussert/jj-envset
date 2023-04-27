19267b475c0972f0d4fd919ce486fd3cbdccf7b4
JJ Bussert <jj.bussert@gmail.com>
2023-04-26 21:27:20 -0500

    extracting command execution to jj-envrun

--- src/envset.js
+++ src/envset.js
@@ -3,11 +3,8 @@
 import fs from 'fs';
 import dotenv from 'dotenv';
 import os from 'os';
-import { execSync } from 'child_process';
 
-function envset(targetKey, command, envFile) {
-    // Execute the command and get its standard output
-    const commandOutput = execSync(command).toString().trim();
+function envset(key, value, envFile) {
 
     // Check if the .env file exists
     const envFileExists = fs.existsSync(envFile);
@@ -23,19 +20,19 @@ function envset(targetKey, command, envFile) {
     }
 
     // Update or create the key-value pair in the dotenvObj
-    if (dotenvObj.hasOwnProperty(targetKey)) {
+    if (dotenvObj.hasOwnProperty(key)) {
         // If the key already exists, update the value
-        console.log(`Updating ${targetKey} in .env file`);
+        console.log(`Updating ${key} in .env file`);
     } else {
         // If the key does not exist, add it to the environment
-        console.log(`Adding ${targetKey} to .env file`);
+        console.log(`Adding ${key} to .env file`);
     }
 
-    dotenvObj[targetKey] = commandOutput;
+    dotenvObj[key] = value;
 
     // Save the environment variable to the .env file
     const env = Object.entries(dotenvObj)
-        .map(([key, value]) => `${key}="${value}"`)
+        .map(([k, v]) => `${k}="${v}"`)
         .join(os.EOL);
     fs.writeFileSync(envFile, env);
 }
--- tests/envset.test.js
+++ tests/envset.test.js
@@ -3,9 +3,9 @@ import { envset } from '../src/envset.js';
 import fs from 'fs';
 import { execSync } from 'child_process';
 
-test('envset: should save the output of the command to the specified key in the env file', async () => {
-  const targetKey = 'HELLO_WORLD';
-  const command = 'echo Hello World!';
+test('envset: should save the key/value pair to the env file', async () => {
+  const key = 'HELLO_WORLD';
+  const value = 'Static Value!';
   const envFile = '.env.test';
 
   // Remove the envFile if it exists
@@ -14,11 +14,11 @@ test('envset: should save the output of the command to the specified key in the 
   }
 
   // Call the envset function
-  envset(targetKey, command, envFile);
+  envset(key, value, envFile);
 
   // Read the updated env file
   const envContent = fs.readFileSync(envFile, 'utf-8');
-  const expectedOutput = `${targetKey}="${execSync(command).toString().trim()}"`;
+  const expectedOutput = `${key}="${value}"`;
 
   // Check if the target key and value are present in the env file
   expect(envContent).toContain(expectedOutput);
@@ -27,43 +27,41 @@ test('envset: should save the output of the command to the specified key in the 
   fs.unlinkSync(envFile);
 });
 
-test('envset: should update an existing env file with new content', async () => {
-  const targetKey = 'NEW_KEY';
-  const command = 'echo Updated Value!';
+test('envset: should update an existing env file with new value', async () => {
+  const key = 'INITIAL_KEY';
+  const value = 'Updated Value!';
   const envFile = '.env.test';
 
   // Create an initial env file with some content
   fs.writeFileSync(envFile, 'INITIAL_KEY="Initial Value"\n');
 
   // Call the envset function to update the env file with new content
-  envset(targetKey, command, envFile);
+  envset(key, value, envFile);
 
   // Read the updated env file
   const envContent = fs.readFileSync(envFile, 'utf-8');
-  const expectedOutput = `${targetKey}="${execSync(command).toString().trim()}"`;
+  const expectedOutput = `${key}="${value}"`;
 
   // Check if the target key and value are present in the env file
   expect(envContent).toContain(expectedOutput);
-  expect(envContent).toContain('INITIAL_KEY="Initial Value"');
 
   // Clean up by removing the envFile
   fs.unlinkSync(envFile);
 });
 
 test('envset: should update an quote existing values if they were not', async () => {
-  const targetKey = 'NEW_KEY';
-  const command = 'echo Updated Value!';
+  const key = 'NEW_KEY';
+  const value = 'New Value!';
   const envFile = '.env.test';
 
-  // Create an initial env file with some content
+  // Create an initial env file with some content with no double quotes
   fs.writeFileSync(envFile, 'INITIAL_KEY=Initial Value\n');
 
   // Call the envset function to update the env file with new content
-  envset(targetKey, command, envFile);
+  envset(key, value, envFile);
 
   // Read the updated env file
   const envContent = fs.readFileSync(envFile, 'utf-8');
-  const expectedOutput = `${targetKey}="${execSync(command).toString().trim()}"`;
 
   // Check if the target key and value are present in the env file
   expect(envContent).toContain('INITIAL_KEY="Initial Value"');
